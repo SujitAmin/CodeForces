a10c05bc37a34742e99e686dfe0fb0567c7904cc
SujitAmin <mastersujitamin@gmail.com>
2018-11-05 18:52:50 +0530

    Commit

---
+++ src/CodeForces702A.java
@@ -0,0 +1,37 @@
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class CodeForces702A {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
+        int[] arr = new int[N];
+        for(int z =0;z<N;z++){
+            arr[z] = Integer.parseInt(temp[z]);
+        }
+        int res = CodeForces(arr,N);
+        System.out.println(res);
+    }
+
+    private static int CodeForces(int[] arr, int n) {
+        int max = 1;
+        int res = 1;
+        for(int i = 0 ; i< n - 1;i++){
+            if(arr[i] < arr[i + 1]){
+                max++;
+            }else{
+                max = 1;
+
+            }
+            if(res < max){
+                res = max;
+            }
+        }
+        return res;
+    }
+}
