e6f11a8842995960fbde5f4ced541d654862f22c
SujitAmin <mastersujitamin@gmail.com>
2018-11-05 12:31:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+// A Naive recursive JAVA program to find minimum of coins 
+// to make a given change V 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main
+{
+    // m is size of coins array (number of different coins)
+    private static int minCoins(int[] coins, int m, int V) {
+        if(V <= 0){
+            return 0;
+        }
+        int coin = 0;
+
+        for(int j = m - 1 ;j>=0;j--){
+                if(V >= coins[j]) {
+                    coin = coin + (V/(coins[j]));
+                    V = V % coins[j];
+                }
+               //coin = 0 + 125 /100
+        }
+
+        return coin;
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+        int res = minCoins(new int[]{1, 5, 10, 20, 100}, 5,N);
+        System.out.println(res);
+    }
+}
